f2e2d2bff7f4033652b287b775a8e903d00dcf17
Steven Collins <steven@Stevens-MacBook-Pro.local>
2019-03-15 15:15:17 -0400

    Removed old starter code, added minor change to login page

---
+++ src/Javascript/Login.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Bike from '../Images/Bike.png';
+
+class Login extends Component {
+    render() {
+        return (
+            <img src={Bike} alt="Bike should be here"/>
+        );
+    }
+}
+
+// class Form extends Component {
+//     render() {
+//         return (
+//             <div>Pizza</div>
+//         );
+//     }
+// }
+
+export default Login;
