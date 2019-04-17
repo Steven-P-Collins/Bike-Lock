3dbc1c4adc1fada68e49a69d9432414de456912a
Nicholas Gaston <nickgaston22@icloud.com>
2019-04-17 01:51:06 -0400

    Updated PHP file to read DB table Rack and create JSON file

--- src/PHP/RackToJSON.php
+++ src/PHP/RackToJSON.php
@@ -11,15 +11,23 @@ if (!$connection) {
     die('Not connected :' . $connection->connect_error);
 }
 
-$data = array();
+$data = array(
+    array()
+);
 $query = "SELECT * FROM Rack";
 $result = $connection->query($query);
 if (!$result) {
     die('Invalid query: ' . $connection->connect_error);
 }
 
+$x = 0;
 while ($row = @mysqli_fetch_assoc($result)) {
-    $data[] = $row;
+    $data[$x][] = $row['name'];
+    $data[$x][] = $row['lat'];
+    $data[$x][] = $row['lng'];
+    $data[$x][] = $row['rackID'];
+
+    $x += 1;
 }
 
 $data = json_encode($data);
