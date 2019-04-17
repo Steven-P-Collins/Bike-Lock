d96d95e03ba38a4b3b9feb3f398776bd95c38650
Steven Collins <steven@Stevens-MacBook-Pro.local>
2019-04-16 23:07:14 -0400

    Removed inforwindow.js

--- src/Javascript/infoWindow.js
+++
@@ -1,9 +0,0 @@
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     let lockButton = document.getElementsByClassName('locks')[0];
-//
-//     lockButton.onclick = () => {
-//         alert('Available locks: ' + availableLocks[lockButton.classList.item(1)][0]);
-//     }
-// });
