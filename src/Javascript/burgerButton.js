44499dc04defb10bc42c923a9c6b45640066ca7b
Steven Collins <steven@Stevens-MacBook-Pro.local>
2019-04-22 16:28:20 -0400

    Changes to side bar and some other shit

--- src/Javascript/burgerButton.js
+++ src/Javascript/burgerButton.js
@@ -4,11 +4,11 @@ window.onload = function () {
         let sidePanel = document.getElementsByClassName('sidePanel')[0].style;
         let searchBar = document.getElementsByClassName('pac-input')[0].style;
 
-        if (sidePanel.width === '20%')  {
+        if (sidePanel.width === '45%')  {
             sidePanel.width = '0';
             searchBar.display = 'block';
         } else {
-            sidePanel.width = '20%';
+            sidePanel.width = '45%';
             searchBar.display = 'none';
         }
 
