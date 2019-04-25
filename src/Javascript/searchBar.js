c6b48e502d94b3229ed4c6d636d12f718121f0ac
Steven Collins <steven@wlan-172-20-41-67.csuohio.edu>
2019-04-25 18:11:48 -0400

    Breaking the whole thing

--- src/Javascript/searchBar.js
+++ src/Javascript/searchBar.js
@@ -122,9 +122,7 @@ function initAutocomplete() {
     //Bug prevents us from using any map movement with other animations
 
     rackLocation.forEach(rack => {
-        alert(rack[3][0]);
-
-        // icons.numbers.url += availableLocks[rack[3]][0] + '.png';
+        icons.numbers.url += availableLocks[rack[3]][0] + '.png';
         let marker = new google.maps.Marker({
             position: {lat: parseFloat(rack[1]), lng: parseFloat(rack[2])},
             map: map,
