619cce0fe8434cc91388a6be4146ed44da187b26
Steven Collins <steven@wlan-172-20-41-67.csuohio.edu>
2019-04-25 17:43:23 -0400

    Reverting

--- src/Javascript/searchBar.js
+++ src/Javascript/searchBar.js
@@ -121,10 +121,8 @@ function initAutocomplete() {
     });
     //Bug prevents us from using any map movement with other animations
 
-    console.log('Racks: ' + rackLocation);
-    console.log('Locks: ' + availableLocks);
-
     rackLocation.forEach(rack => {
+        console.log(rack);
         icons.numbers.url += availableLocks[rack[3]][0] + '.png';
         let marker = new google.maps.Marker({
             position: {lat: rack[1], lng: rack[2]},
