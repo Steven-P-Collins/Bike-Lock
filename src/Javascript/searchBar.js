f94ca17cd1768613505c2bac67cb4e0f37d61e85
Steven Collins <steven@wlan-172-20-41-67.csuohio.edu>
2019-04-25 19:32:13 -0400

    Breaking the whole thing

--- src/Javascript/searchBar.js
+++ src/Javascript/searchBar.js
@@ -119,7 +119,7 @@ function initAutocomplete() {
 
     rackLocation.forEach(rack => {
         alert('rack #: '+rack[3]);
-        alert('Avail lock #: '+availableLocks[0][rack[3]][6]);
+        alert('Avail lock #: '+availableLocks[rack[3]][0][6]);
 
         // icons.numbers.url += availableLocks[rack[3]][0] + '.png';
         let marker = new google.maps.Marker({
