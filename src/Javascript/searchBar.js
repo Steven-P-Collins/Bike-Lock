8c8cbf9220f4a7eddddd2cc14591e28f1814d4cd
Steven Collins <steven@wlan-172-20-41-67.csuohio.edu>
2019-04-25 19:08:55 -0400

    Breaking the whole thing

--- src/Javascript/searchBar.js
+++ src/Javascript/searchBar.js
@@ -121,7 +121,7 @@ function initAutocomplete() {
     });
     //Bug prevents us from using any map movement with other animations
 
-    console.log('avail locks: '+availableLocks[0][0][1]);
+    console.log('avail locks: '+availableLocks[1][0][1]);
     console.log('racks: ' + rackLocation);
     rackLocation.forEach(rack => {
 
